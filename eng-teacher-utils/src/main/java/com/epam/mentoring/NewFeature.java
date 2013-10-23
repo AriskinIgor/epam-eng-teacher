05b0b55dfddc7a306f9b09d630646ff0708e710a
Igor_Ariskin <Igor_Ariskin@epam.com>
2013-10-23 12:56:46 +0400

    [555] [bugfix] [refactoring test]

---
+++ eng-teacher-utils/src/main/java/com/epam/mentoring/NewFeature.java
@@ -0,0 +1,13 @@
+package com.epam.mentoring;
+
+public class NewFeature {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
