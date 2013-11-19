1b07440267c6bd519ca5bf5b02341e464aea8a75
Igor_Ariskin <Igor_Ariskin@epam.com>
2013-11-19 12:43:49 +0400

    Для проверки сборки после коммита.

--- eng-teacher-web/src/main/java/com/epam/mentoring/engteacher/springmvc/controllers/AsyncBean.java
+++ eng-teacher-web/src/main/java/com/epam/mentoring/engteacher/springmvc/controllers/AsyncBean.java
@@ -7,28 +7,27 @@ import javax.servlet.AsyncContext;
 import org.apache.log4j.Logger;
 
 @Stateless
-public class AsyncBean
-  {
-     private Logger logger = Logger.getLogger(AsyncBean.class.getName());
+public class AsyncBean {
+	private Logger logger = Logger.getLogger(AsyncBean.class.getName());
 
-
-     @Asynchronous
-     public void executeLongTask(AsyncContext asyncContext)
-      {
-           String data="";
-           try {
-                 for(int i=0;i<10;i++)
-                  {
-                     String threadName=Thread.currentThread().getName();
-                     logger.info(i+"\t EJB3 executeLongTask(asyncContext) at "+new java.util.Date()+"\t"+threadName);
-                     Thread.sleep(2000);
-                     data=data+"\n"+i+"\t EJB3 executeLongTask(asyncContext) at "+new java.util.Date()+"\t"+threadName;
-                  }
-                 logger.info("[EJB Processing is done] : "+data);
-           asyncContext.complete();
-
-       } catch (Exception e) {
-           logger.info(e.getMessage(), e);
-       }
-      }
-  }
+	@Asynchronous
+	public void executeLongTask(AsyncContext asyncContext) {
+		String data = "";
+		try {
+			for (int i = 0; i < 10; i++) {
+				String threadName = Thread.currentThread().getName();
+				logger.info(i + "\t EJB3 executeLongTask(asyncContext) at "
+						+ new java.util.Date() + "\t" + threadName);
+				Thread.sleep(2000);
+				data = data + "\n" + i
+						+ "\t EJB3 executeLongTask(asyncContext) at "
+						+ new java.util.Date() + "\t" + threadName;
+			}
+			logger.info("[EJB Processing is done] : " + data);
+			asyncContext.complete();
+			System.out.println("Test commit");
+		} catch (Exception e) {
+			logger.info(e.getMessage(), e);
+		}
+	}
+}
