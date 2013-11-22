5412eddf50fe9580352c02796a849083539645a0
Igor_Ariskin <Igor_Ariskin@epam.com>
2013-11-22 17:06:51 +0400

    Added new intergation tests.

---
+++ eng-teacher-web/src/test/java/com/epam/mentoring/engteacher/springmvc/controllers/ControllersITest.java
@@ -0,0 +1,13 @@
+package com.epam.mentoring.engteacher.springmvc.controllers;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class ControllersITest {
+
+	@Test
+	public void testInsertGroup() {
+		Assert.assertEquals("", "");
+	}
+}
